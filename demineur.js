4ef706194c6a1fbdc24488ebbe01ab3692cd6cf6
paul L'Hegarat <paullh@laposte.net>
2023-06-12 00:43:51 +0200

    code

--- demineur.js
+++ demineur.js
@@ -4,3 +4,15 @@ import * as R from "ramda";
 const rows = 5;
 const cols = 5;
 const mineCount = 10;
+
+function generateEmptyGrid(rows, cols) {
+    const grid = [];
+    for (let i = 0; i < rows; i++) {
+        const row = [];
+        for (let j = 0; j < cols; j++) {
+            row.push({ isMine: false, isDiscovered: false, adjacentMines: 0 });
+        }
+        grid.push(row);
+    }
+    return grid;
+}
